1a3d7e3e84856d03b443009abd242d40bcf20af6
Maycon Muller <maycon4muller5@gmail.com>
2019-05-10 00:50:49 -0300

    inclusão do projeto com as telas .html

---
+++ Buscar.js
@@ -0,0 +1,55 @@
+var campoBuscar = document.querySelector("#buscar");
+
+
+campoBuscar.addEventListener("input", function(){
+	console.log(this.value);
+	var Aparelhos = document.querySelectorAll("#IDTrs");
+
+
+/*
+	if( this.value.length >0){
+       for (var i = 0; i<Aparelhos.length; i++) {
+       var IDTrs = Aparelhos[i];
+
+       var tdNome = IDTrs.querySelector(".info-nome");
+       var nome= tdNome.textContent;
+       var expressao= new RegExp(this.value,"i");
+       if( nome!=this.value){
+       	IDTrs.classList.add("invisivel");
+       }else{
+       	IDTrs.classList.remove("invisivel");
+       }
+		}
+	}else{
+		for (var i = 0; i<Aparelhos.length; i++) {
+			var IDTrs = Aparelhos[i];
+			IDTrs.classList.remove("invisivel");
+		}
+	}
+
+});
+*/
+
+
+
+	if( this.value.length >0){
+       for (var i = 0; i<Aparelhos.length; i++) {
+       var IDTrs = Aparelhos[i];
+
+       var tdNome = IDTrs.querySelector(".info-nome");
+       var nome= tdNome.textContent;
+       var expressao= new RegExp(this.value,"i");
+       if( !expressao.test(nome)){
+       	IDTrs.classList.add("invisivel");
+       }else{
+       	IDTrs.classList.remove("invisivel");
+       }
+		}
+	}else{
+		for (var i = 0; i<Aparelhos.length; i++) {
+			var IDTrs = Aparelhos[i];
+			IDTrs.classList.remove("invisivel");
+		}
+	}
+
+});
