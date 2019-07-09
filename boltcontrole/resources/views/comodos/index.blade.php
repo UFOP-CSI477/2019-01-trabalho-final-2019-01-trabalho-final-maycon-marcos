97a0a5d8282de0a6af75b9965198ddce76f723bf
marcosvtn94 <48065127+marcosvtn94@users.noreply.github.com>
2019-07-09 19:19:21 -0300

    commit

--- boltcontrole/resources/views/comodos/index.blade.php
+++ boltcontrole/resources/views/comodos/index.blade.php
@@ -1,6 +1,38 @@
 @extends('principal')
 
 
-@section('name')
+@section('Numero de comodos')
+
+
+  div class="container">
+   <div class="table-responsive">
+
+   <table class="table">
+   <thead>
+     <tr>
+       <th>Id do Comodo:</th>
+       <th>Nome do Comodo:</th>
+     </tr>
+
+   </thead>
+   <tbody>
+     @foreach ($comodo as $c)
+       <tr>
+         <td>{{ $c->id }}</td>
+         <td>{{  $c->nome}}</td>
+         {{-- <td><a href="{{ route('procedures.show', $e->id) }}">{{ $e->name }}</a></td> --}}
+
+        </tr>
+     @endforeach
+
+   </tbody>
+   </table>
+   </div>
+   </div>
+
+
+
+
+
 
 @endsection
