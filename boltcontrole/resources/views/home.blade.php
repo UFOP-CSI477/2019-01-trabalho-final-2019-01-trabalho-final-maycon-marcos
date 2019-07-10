b5d8f46bd8f80f6e31b8ff9bb589dea7f8e92793
Maycon Muller <maycon4muller5@gmail.com>
2019-07-09 23:41:37 -0300

    inclusao_arquivo_js

--- boltcontrole/resources/views/home.blade.php
+++ boltcontrole/resources/views/home.blade.php
@@ -5,25 +5,36 @@
       <div class="row">
         <h1 class="col-12">Dashboard</h2>
           <br><br><br>
-   @if(auth::user()->consumudiario != null)
+   @if(auth::user()->consumudiario == null)
           <h2>Consumo Diário</h2>
 
        <p>Reserve dois dias normais que possam refletir o mais próximo possível das suas atividades habituais para que possamos calcular  o valor de seu consumo diário de acordo com os valores fornecidos no seu medidor da sua companhia de energia!</p>
         <div class="col-6"> <!-- Informações sobre padrão de energia -->
           <form>
-         <div class="form-group">
-         <label for="qnt"><h4>Informe o número estava no seu medidor Ontem</h4></label>
-          <input type="number" name="qnt" id="qnt" placeholder="Valor de ontem" class="form-control sm">
+
+         <div class="form-group" id="grupo1">
+         <label for="valor1" id="labelv1">Informe o número estava no seu medidor Ontem</label>
+          <input type="number" name="valor1"  placeholder="Valor de ontem" class="form-control sm">
+
+          <div id="alerta1" style="display: none;" class="alert alert-danger">
+            <span>Preencha o primeiro valor corretamente!<span>
+          </div>
+
         </div>
 
-        <div class="form-group">
-         <label for="qnt"><h4>Informe o número que esta no seu medidor Hoje</h4></label>
-          <input type="number" name="qnt" id="qnt" placeholder="Valor de hoje" class="form-control sm">
+        <div class="form-group" id="grupo2">
+         <label for="valor2">Informe o número que esta no seu medidor Hoje</label>
+          <input type="number" name="valor2"  placeholder="Valor de hoje" class="form-control sm">
+
+          <div id="alerta2" style="display: none;" class="alert alert-danger">
+            <span>Preencha o segundo  valor corretamente!<span>
+          </div>
         </div>
+
+
 
         <div>
-          <input id="botao" type="reset" value="Limpar" class="btn btn-secondary">
-          <input id="botao" type="submit"  value="Calcular"  name="confirmar" class="btn btn-info">
+          <input  type="submit"  value="Calcular"  name="calculo" id="calculo" class="btn btn-info">
 
             <a href="#" class="btn btn-light">Como ler seu padrão</a>
 
@@ -33,10 +44,10 @@
         </div>
 
         <div class="col-6"  style="background-color: #f3f3f3; border-radius: 10px;"> <!-- Exibição do valor do consumo diário-->
-         <h2>Aqui aparecerá o valor do consumo diário</h2>
+         <h2>valor do consumo diário</h2>
          <br><br>
          <h4>Consumo Carregado em nosso sistema:</h4>
-         <h2>XXXXXXXXXX</h2>
+         <h2 id="resultado"> XXXX </h2>
         </div>
 
       </div> <!-- fim da linha -->
@@ -52,4 +63,6 @@
          </div>
 
     @endif
+
+    <script src="{{asset('js/codigo.js')}}" defer></script>
 @endsection
