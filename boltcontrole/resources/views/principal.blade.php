2b71abde8de8f62fff413095ff0b10544c659d95
Maycon Muller <maycon4muller5@gmail.com>
2019-07-11 19:24:51 -0300

    inclusão_da_fonte

--- boltcontrole/resources/views/principal.blade.php
+++ boltcontrole/resources/views/principal.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- fonte usada no sistema -->
-    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aladin"> -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aladin"> 
       <!-- Arquivo CSS externo -->
     <link rel="stylesheet"  href="../codigos-CSS-JS/estilo.css">
 
